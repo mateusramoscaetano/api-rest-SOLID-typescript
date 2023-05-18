4a9fc323927106e6dbe8ce02cc112eec48c96e9e
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-05-18 11:11:32 -0300

    delelte user implemented

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,18 @@
-import { Router } from "express";
+import { Router, request } from "express";
 import { createUserController } from "./useCases/CreateUser";
 import { updateUserController } from "./useCases/UpdateUser";
+import { deleteUserController } from "./useCases/DeleteUser";
 
 const router = Router();
 
 router.post("/users", (request, response) => {
   return createUserController.handle(request, response);
 });
-router.put("users", (request, response) => {
+router.put("/users", (request, response) => {
   return updateUserController.handleUpdate(request, response);
+});
+router.delete("/users/:id", (request, response) => {
+  return deleteUserController.handle(request, response);
 });
 
 export { router };
---
+++ src/useCases/DeleteUser/DeleteUserUseCaseDTO.ts
@@ -0,0 +1,3 @@
+export class IDeleteUserRequestDTO {
+  id: string;
+}
